a4894fe6ad6c4dd3c2bdbb6037f2c157b8a1e9e0
Alban Nocaj <alban.nocaj@gmail.com>
2017-05-11 17:02:00 +0200

    git changes

---
+++ src/main/java/hello/Blackboard.java
@@ -0,0 +1,46 @@
+package hello;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class Blackboard {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String message;
+
+
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+    
+    
+}
+
