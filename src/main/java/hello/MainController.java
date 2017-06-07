d0373c7b8b3376a1bf01d62ab129713e4b3c83db
Alban Nocaj <alban.nocaj@gmail.com>
2017-06-07 21:23:08 +0200

    changed show_blackboards

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -2,6 +2,7 @@ package hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,9 +27,9 @@ public class MainController {
 	}
 
 	@GetMapping(path = "/show_blackboards")
-	public @ResponseBody Iterable<Blackboard> getAllBlackboards() {
+	public @ResponseBody ResponseEntity  getAllBlackboards() {
 		// This returns a JSON or XML with the Blackboards
-		return blackboardRepository.findAll();
+		return new ResponseEntity(blackboardRepository.findAll(), HttpStatus.OK);
 	}
 
 	@GetMapping(path="/exists_blackboard")
