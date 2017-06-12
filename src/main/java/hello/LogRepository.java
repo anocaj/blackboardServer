916722f65a5e6b181848820ddff58430a5b5ed41
MaBaehr <manubaehr@aol.com>
2017-06-12 19:52:06 +0200

    Correct LogRepo.java

--- src/main/java/hello/LogRepository.java
+++ src/main/java/hello/LogRepository.java
@@ -5,13 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-// trial to implement the Repository class for Log (actually not necessary? only storing is needed for REST-API)
 public interface LogRepository extends CrudRepository<Log, Long> {
 
-//    @Transactional
-//    save(Log entity);
-
-    @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Log b WHERE b.address = :address")
-    boolean existsByAddress(@Param("address") String address);
-
+	// keep empty, since no access via REST-Api needed
+	
 }
