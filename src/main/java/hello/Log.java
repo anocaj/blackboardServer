bf2249b586ca7f3d2dcbf14e2d5e8c66708c8831
MaBaehr <manubaehr@aol.com>
2017-06-12 13:08:33 +0200

    Create provisional LogRepository and update Maven dependencies

--- src/main/java/hello/Log.java
+++ src/main/java/hello/Log.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity // This tells Hibernate to make a table out of this class
 public class Log {
