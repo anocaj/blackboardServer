dcb65b0a58bcbd7df296226108202ae7638bd1f3
Alban Nocaj <alban.nocaj@gmail.com>
2017-06-14 13:40:41 +0200

    updated comments

--- src/main/java/hello/Blackboard.java
+++ src/main/java/hello/Blackboard.java
@@ -5,26 +5,15 @@ import javax.persistence.*;
 @Entity // This tells Hibernate to make a table out of this class
 public class Blackboard {
 
-//	@GeneratedValue(strategy=GenerationType.AUTO)
-// 	private Integer id;
-	@Id
-	@Column(length = 64)
+	@Id // sets primary key on name column
+	@Column(length = 64) // maximal possible name size is set to 64
     private String name;
-	@Column(length = 255)
+
+	@Column(length = 255) // maximal possible name size
     private String message;
 
 
-
-
-
-//	public void setId(Integer id) {
-//		this.id = id;
-//	}
-
-//	public Integer getId() {
-//		return id;
-//	}
-
+    //standard getter and setter methods
 	public String getName() {
 		return name;
 	}
--- src/main/java/hello/BlackboardRepository.java
+++ src/main/java/hello/BlackboardRepository.java
@@ -5,17 +5,22 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// This will be AUTO IMPLEMENTED by Spring into a Bean called blackboardRepository
 // CRUD refers Create, Read, Update, Delete
 
+// The BlackboardRepository implements the Database to Object binding,
+// this means that the MySQL table blackboard entries are bound to Blackboard entries
 public interface BlackboardRepository extends CrudRepository<Blackboard, Long> {
 
+    // returns a Blackboard by using the name as input
     @Transactional
     Blackboard findOneByName(String name);
 
+    // deletes a Blackboard entrie by using the name as input
     @Transactional
     void deleteBlackboardByName(String name);
 
+    // checks on database if specific blackboard exists
     @Query("SELECT CASE WHEN COUNT(b) > 0 THEN true ELSE false END FROM Blackboard b WHERE b.name = :name")
     boolean existsByName(@Param("name") String name);
 
